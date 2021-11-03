870437d327941d1683e55054548236825ed68119
Callum Kindred <callum-luis.kindred@kainos.com>
2021-11-03 12:59:17 +0000

    add jenkins toggle

--- src/test/scala/BasicMothSimulation.scala
+++ src/test/scala/BasicMothSimulation.scala
@@ -1,13 +1,29 @@
 import scala.concurrent.duration._
+import java.util.Properties
+import scala.io.Source
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 class BasicMothSimulation extends Simulation {
 
+  val url = getClass.getResource("application.properties")
+  val properties: Properties = new Properties()
+
+  if (url != null) {
+    val source = Source.fromURL(url)
+    properties.load(source.bufferedReader())
+  }
+  else {
+    println("properties file cannot be loaded")
+  }
+
+  val isJenkins = properties.getProperty("jenkins")
+  val host =  if(isJenkins == "true") "http://host.docker.internal:3007/" else "http://127.0.0.1:3007/"
+  
   val httpProtocol = http
     // Here is the root for all relative URLs
-    .baseUrl("host.docker.internal:3007/")
+    .baseUrl(host)
     // Here are the common headers
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .doNotTrackHeader("1")
